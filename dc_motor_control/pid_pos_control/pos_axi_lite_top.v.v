ee6ba25a6eb8ab6a338ab9cf5a8453ca0fa7cda2
Changdae Kwak <k454564@naver.com>
2025-05-13 15:07:25 +0900

    Add files via upload

---
+++ dc_motor_control/pid_pos_control/pos_axi_lite_top.v.v
@@ -0,0 +1,80 @@
+`timescale 1 ns / 1 ps
+
+module myip_v1_0 #
+(
+    // Parameters for AXI Slave Bus Interface S00_AXI
+    parameter integer C_S00_AXI_DATA_WIDTH = 32,
+    parameter integer C_S00_AXI_ADDR_WIDTH = 4
+)
+(
+    // User-defined ports
+    output [15:0] kp_init,           // Kp 초기 값
+    output [15:0] ki_init,           // Ki 초기 값
+    output [15:0] kd_init,           // Kd 초기 값
+    output [31:0] desired_pos,       // 목표 위치
+    input  [31:0] actual_pos,        // 실제 위치
+
+    // AXI Slave Bus Interface S00_AXI ports
+    input wire s00_axi_aclk,
+    input wire s00_axi_aresetn,
+    input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+    input wire [2 : 0] s00_axi_awprot,
+    input wire s00_axi_awvalid,
+    output wire s00_axi_awready,
+    input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+    input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+    input wire s00_axi_wvalid,
+    output wire s00_axi_wready,
+    output wire [1 : 0] s00_axi_bresp,
+    output wire s00_axi_bvalid,
+    input wire s00_axi_bready,
+    input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+    input wire [2 : 0] s00_axi_arprot,
+    input wire s00_axi_arvalid,
+    output wire s00_axi_arready,
+    output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+    output wire [1 : 0] s00_axi_rresp,
+    output wire s00_axi_rvalid,
+    input wire s00_axi_rready
+);
+
+    // Instantiation of AXI Slave Bus Interface S00_AXI
+    (* dont_touch = "true" *) myip_v1_0_S00_AXI # (
+        .C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+        .C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+    ) myip_v1_0_S00_AXI_inst (
+        // User-defined connections
+        .kp_init(kp_init),               // Kp 초기 값
+        .ki_init(ki_init),               // Ki 초기 값
+        .kd_init(kd_init),               // Kd 초기 값
+        .desired_pos(desired_pos),       // 목표 위치
+        .actual_pos(actual_pos),         // 실제 위치
+
+        // AXI connections
+        .S_AXI_ACLK(s00_axi_aclk),
+        .S_AXI_ARESETN(s00_axi_aresetn),
+        .S_AXI_AWADDR(s00_axi_awaddr),
+        .S_AXI_AWPROT(s00_axi_awprot),
+        .S_AXI_AWVALID(s00_axi_awvalid),
+        .S_AXI_AWREADY(s00_axi_awready),
+        .S_AXI_WDATA(s00_axi_wdata),
+        .S_AXI_WSTRB(s00_axi_wstrb),
+        .S_AXI_WVALID(s00_axi_wvalid),
+        .S_AXI_WREADY(s00_axi_wready),
+        .S_AXI_BRESP(s00_axi_bresp),
+        .S_AXI_BVALID(s00_axi_bvalid),
+        .S_AXI_BREADY(s00_axi_bready),
+        .S_AXI_ARADDR(s00_axi_araddr),
+        .S_AXI_ARPROT(s00_axi_arprot),
+        .S_AXI_ARVALID(s00_axi_arvalid),
+        .S_AXI_ARREADY(s00_axi_arready),
+        .S_AXI_RDATA(s00_axi_rdata),
+        .S_AXI_RRESP(s00_axi_rresp),
+        .S_AXI_RVALID(s00_axi_rvalid),
+        .S_AXI_RREADY(s00_axi_rready)
+    );
+
+    // Add user logic here
+    // 사용자 로직 추가
+
+endmodule
